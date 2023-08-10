2728e5e0cf444a044f9188d89f735c5b88327f97
snjvn <91363279+snjvn@users.noreply.github.com>
2023-08-10 12:27:33 +0530

    use the functions in movements.py to move

---
+++ movements.py
@@ -0,0 +1,15 @@
+def right():
+    #function to turn right
+    pass
+
+def fwd():
+    #function to move forward by one unit
+    pass
+
+def left():
+    #function to turn left
+    pass
+
+def bwd():
+    #function to move back by one unit
+    pass
